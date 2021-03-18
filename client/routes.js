13f0790f9ece47f3d818ec3b0d34c884585a774c
Heather Ha <hyunjung222@gmail.com>
2021-03-18 18:02:46 -0400

    Updates routes.js

--- client/routes.js
+++ client/routes.js
@@ -1,11 +1,9 @@
-
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { withRouter, Route, Switch } from "react-router-dom";
 import PropTypes from "prop-types";
-import { Login, Signup, UserHome, AllLandlords } from "./components";
+import { Login, Signup, UserHome, AllLandlords, Home } from "./components";
 import { me } from "./store";
-
 
 /**
  * COMPONENT
@@ -42,19 +40,19 @@ class Routes extends Component {
 /**
  * CONTAINER
  */
-const mapState = (state) => {
+const mapState = state => {
   return {
     // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
     // Otherwise, state.user will be an empty object, and state.user.id will be falsey
-    isLoggedIn: !!state.user.id,
+    isLoggedIn: !!state.user.id
   };
 };
 
-const mapDispatch = (dispatch) => {
+const mapDispatch = dispatch => {
   return {
     loadInitialData() {
       dispatch(me());
-    },
+    }
   };
 };
 
@@ -67,5 +65,5 @@ export default withRouter(connect(mapState, mapDispatch)(Routes));
  */
 Routes.propTypes = {
   loadInitialData: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired
 };
