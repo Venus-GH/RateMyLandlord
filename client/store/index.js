13d39cc446e5b2e38741f603651049701e6ec82e
Heather Ha <hyunjung222@gmail.com>
2021-03-19 00:57:46 -0400

    Updates landlord summary

--- client/store/index.js
+++ client/store/index.js
@@ -4,8 +4,9 @@ import thunkMiddleware from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import user from "./user";
 import allLandlords from "./landlords";
+import landlord from "./singleLandlord";
 
-const reducer = combineReducers({ user, allLandlords });
+const reducer = combineReducers({ user, allLandlords, landlord });
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 );
