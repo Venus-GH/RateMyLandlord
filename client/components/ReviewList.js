7c224953990c39b872e1981e299c35c1a458c5d6
Calandra Stavroudis <calandra.st@gmail.com>
2021-03-23 20:17:08 -0400

    added thumb up/down functionality

--- client/components/ReviewList.js
+++ client/components/ReviewList.js
@@ -1,65 +1,117 @@
 import React from "react";
 import moment from "moment";
-import { Row, Col, Card, Icon, Chip } from "react-materialize";
+import { Icon, Chip } from "react-materialize";
 
-const ReviewList = ({ reviews }) => {
-  console.log("reviews:", reviews);
-  const grade = { 1: "F", 2: "D", 3: "C", 4: "B", 5: "A" };
-  return (
-    <Col>
-      {reviews.map((review) => {
-        return (
-          <div key={review.id} className="review-card">
-            <div className="review-boxes">
-              <div className="review-box">
-                <div className="box-name">Grade</div>
-                <div className="box-value">{grade[review.grade]}</div>
+class ReviewList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      thumbsUp: 0,
+      thumbsDown: 0,
+    };
+    this.clickThumbUp = this.clickThumbUp.bind(this);
+    this.clickThumbDown = this.clickThumbDown.bind(this);
+  }
+
+  clickThumbUp() {
+    this.setState({
+      thumbsUp: this.state.thumbsUp + 1,
+    });
+  }
+
+  clickThumbDown() {
+    this.setState({
+      thumbsDown: this.state.thumbsDown + 1,
+    });
+  }
+
+  render() {
+    const { reviews } = this.props;
+    console.log("reviews:", reviews);
+    const grade = { 1: "F", 2: "D", 3: "C", 4: "B", 5: "A" };
+    return (
+      <div>
+        {reviews.map((review) => {
+          return (
+            <div key={review.id} className="review-card">
+              <div className="review-boxes">
+                <div className="review-box">
+                  <div className="box-name">Grade</div>
+                  <div className="box-value">{grade[review.grade]}</div>
+                </div>
+                <div className="review-box">
+                  <div className="box-name">Recommend?</div>
+                  <div className="box-value">
+                    {review.wouldRecommend ? "yes" : "no"}
+                  </div>
+                </div>
               </div>
-              <div className="review-box">
-                <div className="box-name">Recommend?</div>
-                <div className="box-value">
-                  {review.wouldRecommend ? "yes" : "no"}
+              <div className="review-body">
+                <div className="review-address-date">
+                  {/* <div className="review-address">{review.address}</div> */}
+                  <div className="review-date">
+                    {moment(review.createdAt).format("LL")}
+                  </div>
+                </div>
+                <div className="review-ratings">
+                  <div>
+                    <span className="review-rating-name">Kindness: </span>
+                    <span className="review-rating-num">{review.kindness}</span>
+                    /5
+                  </div>
+                  <div className="review-rating">
+                    <span className="review-rating-name">Maintenance: </span>
+                    <span className="review-rating-num">
+                      {review.maintenance}
+                    </span>
+                    /5
+                  </div>
+                  <div className="review-rating">
+                    <span className="review-rating-name">Responsiveness: </span>
+                    <span className="review-rating-num">
+                      {review.responsiveness}
+                    </span>
+                    /5
+                  </div>
+                  <div className="review-rating">
+                    <span className="review-rating-name">Pest Control: </span>
+                    <span className="review-rating-num">
+                      {review.pestControl}
+                    </span>
+                    /5
+                  </div>
+                </div>
+                <div className="review-comments">{review.comments}</div>
+                <div className="review-tags">
+                  {review.tags.map((tag) => {
+                    return <Chip key={Math.random() * 100}>{tag}</Chip>;
+                  })}
+                </div>
+                <div className="review-buttons">
+                  <div className="review-thumbs">
+                    <div>
+                      <Icon onClick={this.clickThumbUp}>thumb_up</Icon>
+                      <span>{this.state.thumbsUp}</span>
+                    </div>
+
+                    <div>
+                      <Icon onClick={this.clickThumbDown}>thumb_down</Icon>
+                      <span>{this.state.thumbsDown}</span>
+                    </div>
+                  </div>
+                  <div className="review-mail-flag">
+                    <Icon>email</Icon>
+                    <Icon>flag</Icon>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="review-body">
-              <div className="review-address-date">
-                {/* <div className="review-address">{review.address}</div> */}
-                <div className="review-date">
-                  {moment(review.createdAt).format("LL")}
-                </div>
-              </div>
-              <div className="review-ratings">
-                <div>Kindness: {review.kindness}</div>
-                <div>maintenance: {review.maintenance}</div>
-                <div>responsiveness: {review.responsiveness}</div>
-                <div>pest Control: {review.pestControl}</div>
-              </div>
-              <div className="review-comments">{review.comments}</div>
-              <div className="review-tags">
-                {review.tags.map((tag) => {
-                  return <Chip key={tag.length}>{tag}</Chip>;
-                })}
-                <Chip>slumlord</Chip>
-                <Chip>unresponsive</Chip>
-              </div>
-              <div className="review-buttons">
-                <div className="review-thumbs">
-                  <Icon>thumb_up</Icon>
-                  <Icon>thumb_down</Icon>
-                </div>
-                <div className="review-mail-flag">
-                  <Icon>email</Icon>
-                  <Icon>flag</Icon>
-                </div>
-              </div>
-            </div>
-          </div>
-        );
-      })}
-    </Col>
-  );
-};
+          );
+        })}
+      </div>
+    );
+  }
+}
 
 export default ReviewList;
 
