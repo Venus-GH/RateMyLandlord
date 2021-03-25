e55ef608b1232f906f0efd7fe6a147e8f0f77da3
Nuala O'Donovan <fionnuala.odonovan@gmail.com>
2021-03-25 18:04:13 -0400

    nuala

--- client/store/user.js
+++ client/store/user.js
@@ -1,71 +1,109 @@
-import axios from 'axios'
-import history from '../history'
+import axios from "axios";
+import history from "../history";
 
 /**
  * ACTION TYPES
  */
-const GET_USER = 'GET_USER'
-const REMOVE_USER = 'REMOVE_USER'
-
-/**
- * INITIAL STATE
- */
-const defaultUser = {}
+const GET_USER = "GET_USER";
+const REMOVE_USER = "REMOVE_USER";
+const SET_REVIEWS = "GET_REVIEWS";
+const UPDATE_NAME = "UPDATE_NAME";
 
 /**
  * ACTION CREATORS
  */
-const getUser = user => ({type: GET_USER, user})
-const removeUser = () => ({type: REMOVE_USER})
+const getUser = (user) => ({ type: GET_USER, user });
+const removeUser = () => ({ type: REMOVE_USER });
+const setReviews = (reviews) => ({
+  type: SET_REVIEWS,
+  reviews,
+});
+const updateName = (preferredName) => ({
+  type: UPDATE_NAME,
+  preferredName,
+});
+
+/**
+ * INITIAL STATE
+ */
+const defaultUser = {};
 
 /**
  * THUNK CREATORS
  */
-export const me = () => async dispatch => {
+export const me = () => async (dispatch) => {
   try {
-    const res = await axios.get('/auth/me')
-    dispatch(getUser(res.data || defaultUser))
+    const res = await axios.get("/auth/me");
+    dispatch(getUser(res.data || defaultUser));
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
-}
+};
 
-export const auth = (email, password, method) => async dispatch => {
-  let res
+export const auth = (email, password, method) => async (dispatch) => {
+  let res;
   try {
-    res = await axios.post(`/auth/${method}`, {email, password})
+    res = await axios.post(`/auth/${method}`, { email, password });
   } catch (authError) {
-    return dispatch(getUser({error: authError}))
+    return dispatch(getUser({ error: authError }));
   }
 
   try {
-    dispatch(getUser(res.data))
-    history.push('/home')
+    dispatch(getUser(res.data));
+    history.push("/");
   } catch (dispatchOrHistoryErr) {
-    console.error(dispatchOrHistoryErr)
+    console.error(dispatchOrHistoryErr);
   }
-}
+};
 
-export const logout = () => async dispatch => {
+export const logout = () => async (dispatch) => {
   try {
-    await axios.post('/auth/logout')
-    dispatch(removeUser())
-    history.push('/login')
+    await axios.post("/auth/logout");
+    dispatch(removeUser());
+    history.push("/login");
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
-}
+};
+
+export const getReviews = (id) => async (dispatch) => {
+  try {
+    const { data: reviews } = await axios.get(`/api/users/${id}/reviews`);
+    dispatch(setReviews(reviews));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const updatePreferredName = (id, name) => async (dispatch) => {
+  try {
+    const { data: user } = await axios.put(`/api/users/${id}`, {
+      preferredName: name,
+    });
+    console.log("in updatePreferredName user:", user);
+    dispatch(updateName(user.preferredName));
+  } catch (err) {
+    console.log("there was an error updating preferred name");
+    console.error(err);
+  }
+};
 
 /**
  * REDUCER
  */
-export default function(state = defaultUser, action) {
+export default function (state = defaultUser, action) {
   switch (action.type) {
     case GET_USER:
-      return action.user
+      return action.user;
     case REMOVE_USER:
-      return defaultUser
+      return defaultUser;
+    case SET_REVIEWS:
+      state.reviews = action.reviews;
+      return state;
+    case UPDATE_NAME:
+      state.preferredName = action.preferredName;
+      return state;
     default:
-      return state
+      return state;
   }
 }
