d042ee12113f6cd283237abf60288f8c69a9fafd
Heather Ha <hyunjung222@gmail.com>
2021-03-26 15:38:23 -0400

    Resolves conflict

--- client/components/Loading.js
+++ client/components/Loading.js
@@ -3,7 +3,7 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="loading-screen">
-      <img src="/loading.gif" />
+      <img src="/img/loading.gif" />
     </div>
   );
 };
