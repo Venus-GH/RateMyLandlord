ea5dd58a5ffacacac6a6b3aed92bfe171df03370
Calandra Stavroudis <calandra.st@gmail.com>
2021-03-28 14:25:22 -0400

    added filter function and materialize select multiple bar

--- client/components/AllLandlords.js
+++ client/components/AllLandlords.js
@@ -43,6 +43,27 @@ const orderBy = (order, landlords) => {
   }
 };
 
+const filterBy = (filters, landlords) => {
+  let filteredByGrade = landlords;
+  if (filters.some((filter) => ["A", "B", "C", "D", "F"].includes(filter))) {
+    filteredByGrade = landlords.filter((landlord) =>
+      filters.includes(landlord.avgGrade)
+    );
+  }
+  if (filters.includes("true") && filters.includes("false")) {
+    return filteredByGrade;
+  } else if (filters.includes("true") || filters.includes("false")) {
+    let thisFilter = filters.find((filter) => filter.length > 1);
+    let otherFilter = thisFilter === "true" ? "false" : "true";
+    return filteredByGrade.filter(
+      (landlord) =>
+        landlord.avgWouldRecommend[thisFilter] >
+        landlord.avgWouldRecommend[otherFilter]
+    );
+  }
+  return filteredByGrade;
+};
+
 const rating = (num) => {
   return String(num).length > 1 ? num.toFixed(1) : num;
 };
@@ -51,10 +72,18 @@ class AllLandlords extends React.Component {
   constructor() {
     super();
     this.state = {
+      A: false,
+      B: false,
+      C: false,
+      D: false,
+      F: false,
+      true: false,
+      false: false,
       order: "",
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleOrderChange = this.handleOrderChange.bind(this);
+    this.handleFilterChange = this.handleFilterChange.bind(this);
   }
   componentDidMount() {
     this.props.getLandlords();
@@ -62,6 +91,15 @@ class AllLandlords extends React.Component {
 
   handleChange = (e) => {
     this.props.filter(e.target.value);
+  };
+
+  handleFilterChange = (e) => {
+    e.persist();
+    console.log("event:", e.target);
+    // on click change e.target.value to !value
+    this.setState((prevState) => ({
+      [e.target.value]: !prevState[e.target.value],
+    }));
   };
 
   handleOrderChange = (e) => {
@@ -73,13 +111,16 @@ class AllLandlords extends React.Component {
     if (this.state.order.length > 0) {
       landlords = orderBy(this.state.order, landlords);
     }
+    // if (this.state.filter.length > 1) {
+    //   landlords = filterBy(this.state.filter, landlords);
+    // }
 
     return (
       <div>
         <h3>Landlords</h3>
 
         <div id="order-filter">
-          <Select
+          {/* <Select
             id="Select-9"
             multiple={false}
             onChange={this.handleChange}
@@ -111,7 +152,7 @@ class AllLandlords extends React.Component {
             <option value="3">C</option>
             <option value="4">D</option>
             <option value="5">F</option>
-          </Select>
+          </Select> */}
           <Select
             id="Select-9"
             multiple={false}
@@ -136,7 +177,7 @@ class AllLandlords extends React.Component {
             value=""
           >
             <option disabled value="">
-              Order By
+              Sort By
             </option>
             <option value="grade">Grade</option>
             <option value="kindness">Kindness</option>
@@ -146,10 +187,41 @@ class AllLandlords extends React.Component {
             <option value="most-reviews">Most Reviews</option>
             <option value="least-reviews">Least Reviews</option>
           </Select>
+          <Select
+            id="Select-9"
+            multiple
+            options={{
+              classes: "",
+              dropdownOptions: {
+                alignment: "left",
+                autoTrigger: true,
+                closeOnClick: true,
+                constrainWidth: true,
+                coverTrigger: true,
+                hover: false,
+                inDuration: 150,
+                onCloseEnd: null,
+                onCloseStart: null,
+                onOpenEnd: null,
+                onOpenStart: null,
+                outDuration: 250,
+              },
+            }}
+            value={[""]}
+          >
+            <option disabled value="">
+              Filter Results
+            </option>
+            <option value="A">Grade A</option>
+            <option value="B">Grade B</option>
+            <option value="C">Grade C</option>
+            <option value="D">Grade D</option>
+            <option value="F">Grade F</option>
+            <option value="true">Recommended</option>
+            <option value="false">Not Recommended</option>
+          </Select>
         </div>
-        {landlords.length === 0 && (
-          <div>Sorry, no landlords with that grade.</div>
-        )}
+        {landlords.length === 0 && <div>Sorry, no landlords found.</div>}
 
         <div className=" rowcontainer">
           <div className="row">
