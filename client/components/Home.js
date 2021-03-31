84598d1b97ffa898825dc4729972ac93e38cb6a3
Heather Ha <hyunjung222@gmail.com>
2021-03-31 17:30:05 -0400

    Adds a mention of Explore in FAB

--- client/components/Home.js
+++ client/components/Home.js
@@ -239,8 +239,8 @@ class Home extends Component {
               <div id="helpDiv" className="btn-floating">
                 Welcome! To get started rating your landlord (or finding out
                 about a future landlord), either search above by address or by
-                landlord. If you are new to NYC and just want to learn more
-                about different boroughs?
+                landlord. Are you new to NYC and just want to learn more about
+                different boroughs? <br />
                 <Link to="/explore">Check out our Explore page!</Link>
               </div>
             </li>
