12ed36d86d3395e196303b06948ca7abf44cdae9
Nuala O'Donovan <fionnuala.odonovan@gmail.com>
2021-04-01 16:09:43 -0400

    styling for nav bar, styling of fab, styling for featured listings

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -17,103 +17,100 @@ class Navbar extends React.Component {
   render() {
     const { handleClick, isLoggedIn } = this.props;
     return (
-      <div className="navbar">
-        <nav>
-          <div className="nav-wrapper grey darken-4">
-            <Link to="/" className="brand-logo">
-              Rate My Landlord
-            </Link>
-            <a href="#" data-target="mobile-demo" className="sidenav-trigger">
-              <i className="material-icons white-text">menu</i>
-            </a>
-            {isLoggedIn ? (
-              <ul id="nav-mobile" className="right hide-on-med-and-down">
-                {/* The navbar will show these links after you log in */}
-                <li>
-                  <Link to="/">
-                    <Icon className="white-text">search</Icon>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/explore">Explore</Link>
-                </li>
-                <li>
-                  <Link to="/landlords">Landlords</Link>
-                </li>
-                <li>
-                  <Link to="/account">Account</Link>
-                </li>
-                <li>
-                  <a href="#" onClick={handleClick}>
-                    Logout
-                  </a>
-                </li>
-              </ul>
-            ) : (
-              <ul id="nav-mobile" className="right hide-on-med-and-down">
-                {/* The navbar will show these links before you log in */}
-                <li>
-                  <Link to="/">
-                    <Icon className="white-text">search</Icon>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/explore">Explore</Link>
-                </li>
-                <li>
-                  <Link to="/landlords">Landlords</Link>
-                </li>
-                <li>
-                  <Link to="/login">Login</Link>
-                </li>
-                <li>
-                  <Link to="/signup">Sign Up</Link>
-                </li>
-              </ul>
-            )}
-          </div>
-        </nav>
+      <div className="navbar-fixed">
+        <div className="navbar">
+          <nav>
+            <div className="nav-wrapper grey darken-4">
+              <Link to="/" className="brand-logo">
+                Rate My Landlord
+              </Link>
+              <a href="#" data-target="mobile-demo" className="sidenav-trigger">
+                <i className="material-icons white-text">menu</i>
+              </a>
+              {isLoggedIn ? (
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                  {/* The navbar will show these links after you log in */}
+                  <li>
+                    <Link to="/">
+                      <Icon className="white-text">search</Icon>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/explore">Explore</Link>
+                  </li>
+                  <li>
+                    <Link to="/landlords">Landlords</Link>
+                  </li>
+                  <li>
+                    <Link to="/account">Account</Link>
+                  </li>
+                  <li>
+                    <a href="#" onClick={handleClick}>
+                      Logout
+                    </a>
+                  </li>
+                </ul>
+              ) : (
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                  {/* The navbar will show these links before you log in */}
+                  <li>
+                    <Link to="/">
+                      <Icon className="white-text">search</Icon>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/explore">Explore</Link>
+                  </li>
+                  <li>
+                    <Link to="/landlords">Landlords</Link>
+                  </li>
+                  <li>
+                    <Link to="/login">Login</Link>
+                  </li>
+                  <li>
+                    <Link to="/signup">Sign Up</Link>
+                  </li>
+                </ul>
+              )}
+            </div>
+          </nav>
 
-        <ul className="sidenav" id="mobile-demo">
-          {isLoggedIn && (
+          <ul className="sidenav" id="mobile-demo">
+            {isLoggedIn && (
+              <li>
+                <Link to="/landlords">Landlords</Link>
+              </li>
+            )}
+            <li>
+              <Link to="/explore">Explore</Link>
+            </li>
             <li>
               <Link to="/landlords">Landlords</Link>
             </li>
-          )}
-          {isLoggedIn && (
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-          )}
-          <li>
-            <Link to="/explore">Explore</Link>
-          </li>
-          <li>
-            <Link to="/landlords">Landlords</Link>
-          </li>
-          {isLoggedIn && (
-            <li>
-              <a href="#" onClick={handleClick}>
-                Logout
-              </a>
-            </li>
-          )}
-          {isLoggedIn && (
-            <li>
-              <Link to="/account">Account</Link>
-            </li>
-          )}
-          {!isLoggedIn && (
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          )}
-          {!isLoggedIn && (
-            <li>
-              <Link to="/signup">Sign Up</Link>
-            </li>
-          )}
-        </ul>
+            {isLoggedIn && (
+              <li>
+                <a href="#" onClick={handleClick}>
+                  Logout
+                </a>
+              </li>
+            )}
+            {isLoggedIn && (
+              <li>
+                <Link to="/account">Account</Link>
+              </li>
+            )}
+            {!isLoggedIn && (
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            )}
+            {!isLoggedIn && (
+              <li>
+                <Link to="/signup">Sign Up</Link>
+              </li>
+            )}
+          </ul>
+        </div>
       </div>
     );
   }
